e4443f49dc0509b2403751bd5ab1ea29d55aafee
Indraneel <neilpatilcoldplay@gmail.com>
2022-01-11 12:15:44 -0500

    Updated time capsule page to show graphs

--- time-capsule.js
+++ time-capsule.js
@@ -3,14 +3,35 @@ const response_list = document.querySelector('#ul-responses')
 
 /** Variables */
 class FormInfo {
-    constructor(name,email,response) {
+    constructor(name,email,response,votes) {
         this.name = name;
         this.email = email;
         this.response = response;
+        this.votes = votes;
     }
 }
 
+const objQuestions = [
+    'Robots have replaced humans in any kind of physical manual labour in the workforce',
+    'You have atleast one friend who is a robot',
+    'You own a tesla bot (or an equivalent humanoid) at home who cooks and cleans for you',
+    'You own a fully self driving car (maybe without a steering wheel) and dont remember the last time you actually drove a car',
+    'Your brain has a human computer interface which gives you enhanced cognitive capabilities',
+    'You have artificially intelligent digital twins who can attend online meetings for you',
+    'Robots are allies not enemies',
+    'Super soldiers in the defense forces are robots',
+    'Amazon delivers packages in 15 mins using robots',
+    'Robots play sports and are on TV playing world championships in rugby and football',
+    'Nanobots capable of entering the bloodstream to ‘feed’ cells and extract waste will exist. They will make the normal mode of human food consumption obsolete',
+    'Sports no longer have human referes',
+    'A few of your friends wanted a change and just shifted to mars for a new life'
+]
+
 let formResponses = []
+let formVotes = []
+// Initialise array
+for(let i=0;i<objQuestions.length;i++)
+    formVotes.push(0);
 
 /** Functions */
 function addDummyData() {
@@ -23,15 +44,34 @@ function addDummyData() {
 
 function renderResponses() {
 
+    let listObj = { listvotes : ''}
     let listItems = ''
     for(const response of formResponses)
     {
         listItems += `<li>${response.response}</li>`
         listItems += `<li>${response.name}</li>`
         listItems += `<li><br></li>`
+
+        // Sanity check
+        console.assert(response.votes.length === objQuestions.length)
+
+        // Update votes
+        for(let i=0;i<response.votes.length;i++)
+            formVotes[i] += parseInt(response.votes[i]); 
     }
 
-    response_list.innerHTML = listItems
+    console.log(formVotes)
+
+    // Add bar graphs based on votes
+    for(let i=0;i<objQuestions.length;i++)
+    {
+        listObj.listvotes += `<li> <p>${objQuestions[i]}</p>`
+        addBarGraph(100*(formVotes[i]/formResponses.length),listObj)
+        listObj.listvotes += '</li>'
+        listItems += `<li><br></li>`
+    }
+
+    response_list.innerHTML = listObj.listvotes + `<li><br></li>` + `<li><br></li>` + listItems
 }
 
 const getDataFromJSONbin = async () => {
@@ -58,6 +98,13 @@ async function main() {
     }
 
     console.log('Size of the response array is :',formResponses.length);
+}
+
+function addBarGraph(percentage,listObj)
+{
+    listObj.listvotes += `<div class="bar-container">
+                            <div class="skills" style="width:${percentage}%">${percentage}%</div>
+                        </div> `
 }
 
 /** CODE BEGINS HERE */
